011e29fb8a859437eb8cb4c5ca97c8b9365bf9cf
Vivek Kumar Singh <rogerthatvivek@gmail.com>
2020-07-29 14:23:03 +0530

    Updated package.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,3 @@
-// Learn more on how to config.
-// - https://github.com/ant-tool/atool-build#配置扩展
-
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
   webpackConfig.babel.plugins.push(['import', {
